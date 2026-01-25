4368839c6825dc01b441433de05b3b93d0556410
Aryaman Todkar <aryamant20@gmail.com>
2026-01-26 01:38:01 +0530

    Fixed env issue.

--- apps/web/drizzle.config.ts
+++ apps/web/drizzle.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "drizzle-kit";
 import "dotenv/config";
 
 export default {
-  schema: ["../../packages/db/src/schema/index.ts"],
+  schema: ["packages/db/src/schema/**/*.ts"],
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
