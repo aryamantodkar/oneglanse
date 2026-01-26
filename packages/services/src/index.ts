313f590aacf666558c05c378119a799015ac7d35
Aryaman Todkar <aryamant20@gmail.com>
2026-01-26 15:12:36 +0530

    Fixed imports.

--- packages/services/src/index.ts
+++ packages/services/src/index.ts
@@ -2,4 +2,3 @@ export * from './analysis/analysis';
 export * from './organization/getActiveOrganization';
 export * from './prompt/prompt';
 export * from './workspace/workspace';
-export * from './search/runWebSearch';
