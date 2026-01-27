50277db806800a81a37d4191c7441598ee79dea4
Aryaman Todkar <aryamant20@gmail.com>
2026-01-27 21:28:25 +0530

    Fixed dockerized agent code.

--- apps/agent/src/queue.ts
+++ apps/agent/src/queue.ts
@@ -2,7 +2,7 @@ import { Queue } from "bullmq";
 
 export const agentQueue = new Queue("onescope-agent", {
   connection: {
-    host: process.env.REDIS_HOST || "localhost",
+    host: process.env.REDIS_HOST || "redis",
     port: process.env.REDIS_PORT ? parseInt(process.env.REDIS_PORT, 10) : 6379,
     password: process.env.REDIS_PASSWORD,
   },
