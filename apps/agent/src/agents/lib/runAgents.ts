cad0163778b7fa88f7c01079fd6ef6ca8e0feb27
Aryaman Todkar <aryamant20@gmail.com>
2026-02-01 20:00:14 +0530

    Removed new chat check.

--- apps/agent/src/agents/lib/runAgents.ts
+++ apps/agent/src/agents/lib/runAgents.ts
@@ -2,18 +2,11 @@ import { Page } from "playwright";
 import { runPrompts } from "./runPrompts.js";
 import { warmUpEditor } from "../../lib/input/warmUpEditor.js";
 import { runStep } from "../../lib/utils/runStep.js";
-import { ensureNewChat } from "../openai/lib/newChat.js";
 import { Provider, AskPromptResult, PromptPayload } from "@onescope/types";
 
 export async function runAgents(prompts: PromptPayload, page: Page, provider: Provider): Promise<AskPromptResult[]> {
-    await page.waitForTimeout(5000);
-    
-    if(provider=="openai"){
-      await runStep("Ensuring new chat is created.", page, async () => {
-        await ensureNewChat(page);
-      });
-    }
-  
+    page.waitForTimeout(3000);
+
     await runStep(`Warming up ${provider}`, page, async () => {
       await warmUpEditor(page);
     });
