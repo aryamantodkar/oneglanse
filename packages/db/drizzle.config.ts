1f1cc294e1088c91450c3e83ab10d0a3f8f3331a
Aryaman Todkar <aryamant20@gmail.com>
2026-02-10 23:57:02 +0530

    Fixed database migration issue.

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "drizzle-kit";
 
 export default {
   schema: ["./src/schema/index.ts"],
+  out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
