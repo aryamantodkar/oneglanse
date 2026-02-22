caa1e1b7456d63ba8c8c6d1879c1cb35e09fe3cc
Aryaman Todkar <aryamant20@gmail.com>
2026-02-22 19:19:13 +0530

    Fixed clickhouse validation error.

--- packages/db/src/config/clickhouse.ts
+++ packages/db/src/config/clickhouse.ts
@@ -1,5 +1,7 @@
-if (process.env.NODE_ENV === "production") {
-	if (!process.env.CLICKHOUSE_URL) throw new Error("CLICKHOUSE_URL is not defined");
+if (
+	process.env.NODE_ENV === "production" &&
+	process.env.NEXT_PHASE !== "phase-production-build"
+) {
 	if (!process.env.CLICKHOUSE_USER) throw new Error("CLICKHOUSE_USER is not defined");
 	if (!process.env.CLICKHOUSE_PASSWORD) throw new Error("CLICKHOUSE_PASSWORD is not defined");
 	if (!process.env.CLICKHOUSE_DB) throw new Error("CLICKHOUSE_DB is not defined");
